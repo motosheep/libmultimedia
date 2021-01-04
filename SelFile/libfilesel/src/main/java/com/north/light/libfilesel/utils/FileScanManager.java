1cb5a58c237bfb5add434a1ca6ffb958367d6791
li <2487177596@qq.com>
2021-01-04 19:18:35 +0800

    1多线程文件扫描

--- SelFile/libfilesel/src/main/java/com/north/light/libfilesel/utils/FileScanManager.java
+++ SelFile/libfilesel/src/main/java/com/north/light/libfilesel/utils/FileScanManager.java
@@ -13,12 +13,11 @@ import com.north.light.libfilesel.bean.FileSelParams;
 import java.io.File;
 import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
-import java.util.Objects;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * Created by lzt
@@ -35,8 +34,10 @@ public class FileScanManager implements Serializable {
     private ScanFileListener mListener;
     //扫描结果
     private ConcurrentMap<String, List<FileInfo>> mScanResult = new ConcurrentHashMap<>();
-    //扫描时线程标识
-    private Map<String, Runnable> mScanTAG = new HashMap<>();
+    //u最大线程数量
+    private int MAX_THREAD_COUNT = 10;
+    //线程计数器
+    private AtomicInteger mThreadCounter = new AtomicInteger(0);
 
     private static final class SingleHolder {
         static final FileScanManager mInstance = new FileScanManager();
@@ -105,25 +106,48 @@ public class FileScanManager implements Serializable {
                 mListener.error("扫描线程错误，停止扫描");
             return;
         }
-        //开始扫描，递归
-        if (mScanTAG.get(path) != null) {
-            mIOHandler.removeCallbacks(Objects.requireNonNull(mScanTAG.get(path)));
-            mScanTAG.remove(path);
-        }
-        getDataMap(path).clear();
-        Runnable scanRunnable = new Runnable() {
-            @Override
-            public void run() {
-                getDirList(new File(path), path);
-                if (mListener != null) {
-                    mListener.scanResult(getDataMap(path));
-                }
-                //扫描完成，移除runnable
-                mScanTAG.remove(path);
+        scan(path);
+    }
+
+    /**
+     * 扫描--通过数据集合，平局分配对应的线程任务
+     */
+    private void scan(final String path) {
+        try {
+            mIOHandler.removeCallbacksAndMessages(null);
+            getDataMap(path).clear();
+            File[] files = new File(path).listFiles();
+            final List<List<File>> result = splitList(Arrays.asList(files), MAX_THREAD_COUNT);
+            //启动多线程进行扫描
+            for (int i = 0; i < MAX_THREAD_COUNT; i++) {
+                //多线程
+                final int finalI = i;
+                mIOHandler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        final List<File> cacheFile = result.get(finalI);
+                        final List<File> arrList = new ArrayList(cacheFile);
+                        while (arrList.size() > 0) {
+                            listFile(arrList.get(arrList.size() - 1), path);
+                            arrList.remove(arrList.size() - 1);
+                        }
+                        //已经没有需要扫描的目录
+                        mIOHandler.removeCallbacks(this);
+                        mThreadCounter.decrementAndGet();
+                        if(mThreadCounter.get()==0){
+                            //扫描完成
+                            mListener.scanResult(getDataMap(path));
+                        }
+                    }
+                });
+                Log.d(TAG, "开启线程： " + i);
+                mThreadCounter.incrementAndGet();
             }
-        };
-        mScanTAG.put(path, scanRunnable);
-        mIOHandler.post(scanRunnable);
+        } catch (Exception e) {
+            if (mListener != null) {
+                mListener.scanResult(getDataMap(path));
+            }
+        }
     }
 
     /**
@@ -140,38 +164,50 @@ public class FileScanManager implements Serializable {
         return mScanResult.get(key);
     }
 
-    /**
-     * 扫描
-     */
-    private void getDirList(File file, String originalPath) {
-        listFile(file, originalPath);
-    }
-
     private void listFile(File file, String originalPath) {
         File[] files = file.listFiles();
         try {
-            for (File f : files) {
-                if (!f.isDirectory()) {
-                    FileSelParams params = FileManager.getInstance().getParams();
-                    String format = f.getName().substring(f.getName().lastIndexOf(".") + 1).toLowerCase();
-                    if (params != null && params.getMFormat().contains(format)) {
-                        FileInfo info = new FileInfo();
-                        info.setFileName(f.getName());
-                        info.setFilePath(f.getAbsolutePath());
-                        info.setFileModifyDate(f.lastModified());
-                        info.setFileLength(f.length());
-                        info.setFileParentPath(f.getParent());
-                        getDataMap(originalPath).add(info);
-                        Log.e(TAG, "加入文件路径：" + info.getFileName());
+            if (files != null)
+                for (File f : files) {
+                    if (!f.isDirectory()) {
+                        FileSelParams params = FileManager.getInstance().getParams();
+                        String format = f.getName().substring(f.getName().lastIndexOf(".") + 1).toLowerCase();
+                        if (params != null && params.getMFormat().contains(format)) {
+                            FileInfo info = new FileInfo();
+                            info.setFileName(f.getName());
+                            info.setFilePath(f.getAbsolutePath());
+                            info.setFileModifyDate(f.lastModified());
+                            info.setFileLength(f.length());
+                            info.setFileParentPath(f.getParent());
+                            getDataMap(originalPath).add(info);
+                            Log.e(TAG, "加入文件路径：" + info.getFileName());
+                        }
+                    } else if (f.isDirectory()) {
+                        //如果是目录，迭代进入该目录
+                        listFile(f, originalPath);
                     }
-                } else if (f.isDirectory()) {
-                    //如果是目录，迭代进入该目录
-                    listFile(f, originalPath);
                 }
-            }
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    /**
+     * 分割List
+     */
+    public static <T> List<List<T>> splitList(List<T> list, int groupSize) {
+        int length = list.size();
+        // 计算可以分成多少组
+        int num = (length + groupSize - 1) / groupSize; // TODO
+        List<List<T>> newList = new ArrayList<>(num);
+        for (int i = 0; i < num; i++) {
+            // 开始位置
+            int fromIndex = i * groupSize;
+            // 结束位置
+            int toIndex = (i + 1) * groupSize < length ? (i + 1) * groupSize : length;
+            newList.add(list.subList(fromIndex, toIndex));
+        }
+        return newList;
     }
 
     /**
