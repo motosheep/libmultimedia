ac75332cf81779ca3cfcf8bb7615f8b70b73cc99
li <2487177596@qq.com>
2021-01-06 14:59:34 +0800

    1完成多线程扫描--10s

--- SelFile/libfilesel/src/main/java/com/north/light/libfilesel/utils/FileScanManager.java
+++ SelFile/libfilesel/src/main/java/com/north/light/libfilesel/utils/FileScanManager.java
@@ -10,6 +10,7 @@ import com.north.light.libfilesel.FileManager;
 import com.north.light.libfilesel.bean.FileInfo;
 import com.north.light.libfilesel.bean.FileScanInfo;
 import com.north.light.libfilesel.bean.FileSelParams;
+import com.north.light.libfilesel.thread.FileThreadManager;
 
 import org.jetbrains.annotations.NotNull;
 
@@ -18,6 +19,7 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
 
 /**
@@ -33,11 +35,15 @@ public class FileScanManager implements Serializable, FileScanManagerInterface {
     private Context mContext;
     //监听
     private ScanFileListener mListener;
-    //u最大线程数量
-    private int MAX_THREAD_COUNT = 10;
-    //线程计数器
-    private AtomicInteger mThreadCounter = new AtomicInteger(0);
+    //一个List的大小--分割list
+    private int MAX_LIST_COUNT = 10;
 
+    //计数器--管理线程数量大小
+    private static AtomicInteger mThreadCounter = new AtomicInteger(0);
+    private static AtomicInteger mTotalCounter = new AtomicInteger(0);
+    private static AtomicBoolean mNewThreadTAG = new AtomicBoolean(true);
+    private static AtomicInteger mNumCounter = new AtomicInteger(0);
+    private final int THREAD_COUNT = 20;
 
     private static final class SingleHolder {
         static final FileScanManager mInstance = new FileScanManager();
@@ -114,36 +120,61 @@ public class FileScanManager implements Serializable, FileScanManagerInterface {
         }
         //扫描--通过数据集合，平局分配对应的线程任务
         try {
+            FileThreadManager.getInstance().closeCacheExecutors("");
             mIOHandler.removeCallbacksAndMessages(null);
             FileScanInfo.Companion.getDataMap(path).clear();
             File[] files = new File(path).listFiles();
-            final List<List<File>> result = ListSpilt.splitList(Arrays.asList(files), MAX_THREAD_COUNT);
+            final List<List<File>> result = ListSpilt.splitList(Arrays.asList(files), MAX_LIST_COUNT);
             final List<List<File>> finalList = new ArrayList(result);
-            //启动多线程进行扫描
-            for (int i = 0; i < MAX_THREAD_COUNT; i++) {
-                //多线程
-                final int finalI = i;
-                mIOHandler.post(new Runnable() {
-                    @Override
-                    public void run() {
-                        final List<File> cacheFile = finalList.get(finalI);
-                        final List<File> arrList = new ArrayList(cacheFile);
-                        while (arrList.size() > 0) {
-                            listFile(arrList.get(arrList.size() - 1), path);
-                            arrList.remove(arrList.size() - 1);
+            mTotalCounter.set(0);
+            mThreadCounter.set(0);
+            mNumCounter.set(0);
+            mNewThreadTAG.set(true);
+            Log.e(TAG, "cachePos开始---");
+            mIOHandler.post(new Runnable() {
+                @Override
+                public void run() {
+                    while (true) {
+                        try {
+                            Thread.sleep(20);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
                         }
-                        //已经没有需要扫描的目录
-                        mIOHandler.removeCallbacks(this);
-                        mThreadCounter.decrementAndGet();
-                        if (mThreadCounter.get() == 0) {
-                            //扫描完成
-                            mListener.scanResult(FileScanInfo.Companion.getDataMap(path));
+                        if (mThreadCounter.get() < THREAD_COUNT) {
+                            if (!mNewThreadTAG.get()) {
+                                continue;
+                            }
+                            mNewThreadTAG.set(false);
+                            mThreadCounter.incrementAndGet();
+                            if (mNumCounter.get() == finalList.size()) {
+                                continue;
+                            }
+                            FileThreadManager.getInstance().getCacheExecutors("").execute(
+                                    new ScanRunnable(finalList.get(mNumCounter.getAndIncrement()), path, new ScanRunnable.FinishCallback() {
+                                        @Override
+                                        public void finish() {
+                                            mThreadCounter.decrementAndGet();
+                                            mTotalCounter.incrementAndGet();
+                                            //执行到最后的判断
+                                            if (mTotalCounter.get() == finalList.size()) {
+                                                //至此，全部扫描完毕
+                                                if (mListener != null) {
+                                                    mListener.scanResult(FileScanInfo.Companion.getDataMap(path));
+                                                }
+                                                Log.e(TAG, "cachePos扫描完---");
+                                            }
+                                        }
+
+                                        @Override
+                                        public void init() {
+                                            //已经初始化完成
+                                            mNewThreadTAG.set(true);
+                                        }
+                                    }));
                         }
                     }
-                });
-                Log.d(TAG, "开启线程： " + i);
-                mThreadCounter.incrementAndGet();
-            }
+                }
+            });
         } catch (Exception e) {
             if (mListener != null) {
                 mListener.scanResult(FileScanInfo.Companion.getDataMap(path));
@@ -152,9 +183,39 @@ public class FileScanManager implements Serializable, FileScanManagerInterface {
     }
 
     /**
+     * handler runnable
+     */
+    private static class ScanRunnable implements Runnable {
+        private List<File> scanPath;
+        private String path;
+        private FinishCallback listener;
+
+        public ScanRunnable(List<File> file, String path, FinishCallback callback) {
+            this.scanPath = file;
+            this.path = path;
+            this.listener = callback;
+            this.listener.init();
+        }
+
+        @Override
+        public void run() {
+            for (File file : scanPath) {
+                listFile(file, path);
+            }
+            listener.finish();
+        }
+
+        interface FinishCallback {
+            void finish();
+
+            void init();
+        }
+    }
+
+    /**
      * list file递归
      */
-    private void listFile(File file, String originalPath) {
+    private static void listFile(File file, String originalPath) {
         File[] files = file.listFiles();
         try {
             if (files != null)
@@ -170,7 +231,7 @@ public class FileScanManager implements Serializable, FileScanManagerInterface {
                             info.setFileLength(f.length());
                             info.setFileParentPath(f.getParent());
                             FileScanInfo.Companion.getDataMap(originalPath).add(info);
-                            Log.e(TAG, "加入文件路径：" + info.getFileName());
+                            Log.d(TAG, "加入文件路径：" + info.getFileName());
                         }
                     } else if (f.isDirectory()) {
                         //如果是目录，迭代进入该目录
